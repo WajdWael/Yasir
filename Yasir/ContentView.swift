690e4f8d61a031e040d7193efffb90e6fffb3b73
Wajdwael <wajdabdelhai@gmail.com>
2025-03-04 14:27:24 +0300

    Initial Commit

---
+++ Yasir/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yasir
+//
+//  Created by Wajd Wael on 04/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yasir/YasirApp.swift
@@ -0,0 +1,17 @@
+//
+//  YasirApp.swift
+//  Yasir
+//
+//  Created by Wajd Wael on 04/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct YasirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
